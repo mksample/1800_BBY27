6944af2e1a0177251727e3e3d1fb2f0b0c8b8f20
Monica Bacatan <mbacatan1@my.bcit.ca>
2021-11-01 15:09:40 -0700

    Firebase configuration

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCGnBxy-E5PKAM3O0WHTX4Mgpj8kIwWNT0",
+    authDomain: "bby27-noteapp.firebaseapp.com",
+    projectId: "bby27-noteapp",
+    storageBucket: "bby27-noteapp.appspot.com",
+    messagingSenderId: "816627426282",
+    appId: "1:816627426282:web:70217b9e41fddda46d4893"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
